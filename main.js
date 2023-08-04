d37367d911b9f3cb78aca0716bb5384d3c2b0a19
albertantony911 <66047048+albertantony911@users.noreply.github.com>
2023-08-04 15:11:55 +0530

    Fixed the menu top issue

--- main.js
+++ main.js
@@ -1,29 +1,17 @@
-console.clear();
+function toggleClass(element, stringClass) {
+    if (element.classList.contains(stringClass)) {
+        element.classList.remove(stringClass);
+    } else {
+        element.classList.add(stringClass);
+    }
+}
 
-const app = (() => {
-	let body;
-	let menu;
-	let menuItems;
-	
-    const init = () => {
-        body = document.querySelector('body');
-        menu = document.querySelector('.menu-icon');
-        menuItems = document.querySelectorAll('.nav__list-item');
+function init() {
+    let body = document.querySelector('body');
+    let menu = document.querySelector('.menu-icon');
+    let menuItems = document.querySelectorAll('.nav__list-item');
 
-        applyListeners();
-    };
-	
-    const applyListeners = () => {
-        menu.addEventListener('click', () => toggleClass(body, 'nav-active'));
-    };
-	
-    const toggleClass = (element, stringClass) => {
-        if (element.classList.contains(stringClass))
-            element.classList.remove(stringClass);
-        else
-            element.classList.add(stringClass);
-    };
-	
-	init();
-})();
+    menu.addEventListener('click', () => toggleClass(body, 'nav-active'));
+}
 
+init();
